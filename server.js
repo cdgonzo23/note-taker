f22fbdd49764590ee6ef6a4d2d466d7f37d5d43b
Christian Gonzales <124095890+cdgonzo23@users.noreply.github.com>
2023-07-06 15:28:53 -0700

    added id for notes so there is navigation capability between notes

--- server.js
+++ server.js
@@ -2,66 +2,69 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT ?? 3001;
 const app = express();
+const fs = require('fs')
 const notesData = require('./db/db.json')
+const uuid = require ('./helper/uuid');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/notes', (req, res) => {
     return res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
-// app.get('/api/notes', (req, res) => {
-//     res.json(notesData);
-//     console.info(`${req.method} request recieved to get notes`)
-// });
+app.get('/api/notes', (req, res) => {
+    res.json(notesData);
+    console.info(`${req.method} request recieved to get notes`)
+});
 
 app.get('*', (req, res) => {
     return res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-// app.post('/api/notes', (req, res) => {
-//     console.info(`${req.method} request received to add a new note`);
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a new note`);
 
-//     const { title, text } = req.body;
+    const { title, text } = req.body;
 
-//     if (title && text) {
-//         const newNote = {
-//             title,
-//             text
-//         }
-//         fs.readFile('./db/db.json', 'utf8', (err, data) => {
-//             if (err) {
-//               console.error(err);
-//             } else {
-//               // Convert string into JSON object
-//               const parsedNotes = JSON.parse(data);
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuid()
+        }
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+              console.error(err);
+            } else {
+              // Convert string into JSON object
+              const parsedNotes = JSON.parse(data);
       
-//               // Add a new review
-//               parsedNotes.push(newNote);
+              // Add a new review
+              parsedNotes.push(newNote);
       
-//               // Write updated reviews back to the file
-//               fs.writeFile(
-//                 './db/db.json',
-//                 JSON.stringify(parsedNotes, null, 4),
-//                 (writeErr) =>
-//                   writeErr
-//                     ? console.error(writeErr)
-//                     : console.info('Successfully updated reviews!')
-//               );
-//             }
-//           });
-//         const response = {
-//             status: 'Success',
-//             body: newNote
-//         }
-//         console.log(response)
-//         res.status(201).json(response);
-//     } else {
-//         res.status(500).json('Error in creating new note.')
-//     }
-// });
+              // Write updated reviews back to the file
+              fs.writeFile(
+                './db/db.json',
+                JSON.stringify(parsedNotes, null, 4),
+                (writeErr) =>
+                  writeErr
+                    ? console.error(writeErr)
+                    : console.info('Successfully updated notes!')
+              );
+            }
+          });
+        const response = {
+            status: 'Success',
+            body: newNote
+        }
+        console.log(response)
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error in creating new note.')
+    }
+});
 
 
 app.listen(PORT, () => {
